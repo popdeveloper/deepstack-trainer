665261d485e3b710d809677cfca67f36ee7e4385
Ajay <popdeveloper@gmail.com>
2023-04-14 15:36:05 +0530

    img4 none

--- utils/datasets.py
+++ utils/datasets.py
@@ -624,6 +624,7 @@ def load_mosaic(self, index):
 
     labels4 = []
     s = self.img_size
+    img4 = None
     yc, xc = [int(random.uniform(-x, 2 * s + x)) for x in self.mosaic_border]  # mosaic center x, y
     indices = [index] + [self.indices[random.randint(0, self.n - 1)] for _ in range(3)]  # 3 additional image indices
     for i, index in enumerate(indices):
@@ -634,10 +635,11 @@ def load_mosaic(self, index):
         except:
             pass
 
-        if img is None or img4 is None:
+        if img is None:
             continue
 
         # place img in img4
+        
         if i == 0:  # top left
             img4 = np.full((s * 2, s * 2, img.shape[2]), 114, dtype=np.uint8)  # base image with 4 tiles
             x1a, y1a, x2a, y2a = max(xc - w, 0), max(yc - h, 0), xc, yc  # xmin, ymin, xmax, ymax (large image)
@@ -651,7 +653,8 @@ def load_mosaic(self, index):
         elif i == 3:  # bottom right
             x1a, y1a, x2a, y2a = xc, yc, min(xc + w, s * 2), min(s * 2, yc + h)
             x1b, y1b, x2b, y2b = 0, 0, min(w, x2a - x1a), min(y2a - y1a, h)
-
+        if img4 is None:
+            continue
         img4[y1a:y2a, x1a:x2a] = img[y1b:y2b, x1b:x2b]  # img4[ymin:ymax, xmin:xmax]
         padw = x1a - x1b
         padh = y1a - y1b
